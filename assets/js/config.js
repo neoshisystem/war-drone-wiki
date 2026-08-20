a53ff8f8dbbc49cd3c24770b20041031159f272e
neoshisystem <122578387+neoshisystem@users.noreply.github.com>
2026-08-20 18:45:11 +0330

    Add central wiki configuration

---
+++ assets/js/config.js
@@ -0,0 +1,9 @@
+window.WAR_DRONE_WIKI_CONFIG={
+clanName:"PERSIA",
+projectAuthor:"Hisystem",
+projectTitle:"War Drone Clan Mini-Wiki",
+defaultTheme:"dark",
+locale:"fa-IR",
+stageMin:1,
+stageMax:100
+};
