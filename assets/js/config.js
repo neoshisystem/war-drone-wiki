549d85c961519d4bae9d8449c92188932ec3522e
neoshisystem <122578387+neoshisystem@users.noreply.github.com>
2026-08-21 02:07:22 +0330

    Rename project identity from mini-wiki to PERSIA reference

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,7 +1,7 @@
 window.WAR_DRONE_WIKI_CONFIG={
 clanName:"PERSIA",
 projectAuthor:"Hisystem",
-projectTitle:"War Drone Clan Mini-Wiki",
+projectTitle:"War Drone PERSIA Reference",
 defaultTheme:"dark",
 locale:"fa-IR",
 stageMin:1,
